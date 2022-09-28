37e833bc3535a3995b5495a23839c02ef47391c3
Benoit Gaudet <bgaudet38@gmail.com>
2022-09-28 15:30:40 +0200

    ajout class

---
+++ ServerSide/src/Connection.java
@@ -0,0 +1,11 @@
+import exceptions.SignInFailed;
+
+import java.lang.reflect.Array;
+
+public class Connection {
+    Array clientList;
+
+    boolean signIn(String mail, String pwd) throws SignInFailed {
+        return false;
+    }
+}
