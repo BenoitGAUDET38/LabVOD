24557081fcb35ec26fc41a1d6dae5cf8e3344e12
Benoit Gaudet <bgaudet38@gmail.com>
2022-09-28 16:27:17 +0200

    trucs

--- ServerSide/src/Main.java
+++ ServerSide/src/Main.java
@@ -1,3 +1,5 @@
+import application.Connection;
+
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
--- ServerSide/src/interfaces/IVODService.java
+++ ServerSide/src/interfaces/IVODService.java
@@ -1,8 +1,12 @@
 package interfaces;
 
+import application.Bill;
+import application.MovieDesc;
+
 import java.rmi.RemoteException;
+import java.util.List;
 
 public interface IVODService {
-    ist<MovieDesc> viewCatalog() throws RemoteException;
+    List<MovieDesc> viewCatalog() throws RemoteException;
     Bill playMovie(String isbn, IClientBox box) throws RemoteException;
 }
