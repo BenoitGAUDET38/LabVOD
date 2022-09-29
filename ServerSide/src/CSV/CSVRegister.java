787f66b24b4e37fa55b7f1f2ae24566b346f0121
Thomas Billequin <billequin.thomas@gmail.com>
2022-09-29 22:01:28 +0200

    fix du csv

--- ServerSide/src/CSV/CSVRegister.java
+++ ServerSide/src/CSV/CSVRegister.java
@@ -1,21 +1,25 @@
 package CSV;
 
-import java.io.*;
-import java.util.Scanner;
+import java.io.FileWriter;
+import java.io.IOException;
 
-public class CSVRegister extends CSVReader{
-
-    private String SPACE = " ";
-    private String LINE = "\n";
-
+public class CSVRegister extends CSVLogin {
     public CSVRegister(){
         super();
     }
 
     public boolean add(String mail, String pwd) {
-        if(isCSVContain(mail,pwd)) return false;
+        if(isCSVContain(mail)) return false;
 
         addData(mail + SPACE + pwd + LINE);
         return true;
+    }
+
+    void addData(String text){
+        try {
+            new FileWriter(fileName, true).append(text).close();
+        }catch (IOException io){
+            io.printStackTrace();
+        }
     }
 }
